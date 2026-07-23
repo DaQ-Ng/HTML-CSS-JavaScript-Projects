8a047e0db68ee2f64b43ff4971df626bb472517f
Dat Nguyen <datnguyen1035@gmail.com>
2026-07-23 14:06:19 -0500

    Add animation for dropdown menu

--- Practice/script.js
+++ Practice/script.js
@@ -2,14 +2,18 @@ const navLinks = document.querySelector(".nav-links");
 const menuButton = document.getElementById("menu-btn");
 
 menuButton.addEventListener("click", () => {
-  navLinks.classList.toggle("active"); //adds the class if it's not there, removes it if it is — like a light switch
-
   //swap icon
   if (navLinks.classList.contains("active")) {
-    menuButton.classList.remove("fa-plus");
-    menuButton.classList.add("fa-xmark");
+    navLinks.classList.add("closing");
+
+    setTimeout(() => {
+      navLinks.classList.remove("active");
+      navLinks.classList.remove("closing");
+    }, 300);
   } else {
-    menuButton.classList.remove("fa-xmark");
-    menuButton.classList.add("fa-plus");
+    navLinks.classList.add("active");
   }
+
+  menuButton.classList.toggle("fa-plus");
+  menuButton.classList.toggle("fa-xmark");
 });
