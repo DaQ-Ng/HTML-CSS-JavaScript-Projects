fc722b9678c664fcb4a8964ef392b50b6b428981
Dat Nguyen <datnguyen1035@gmail.com>
2026-07-28 15:27:51 -0500

    Finish styling, dropdown menu setup, and animation

--- Practice/Career/script.js
+++ Practice/Career/script.js
@@ -2,7 +2,16 @@ const menuButton = document.getElementById("menu-button");
 const navLinks = document.querySelector(".nav-links");
 
 menuButton.addEventListener("click", () => {
-  navLinks.classList.add("active");
+  if (navLinks.classList.contains("active")) {
+    navLinks.classList.add("closing");
+
+    setTimeout(() => {
+      navLinks.classList.remove("closing");
+      navLinks.classList.remove("active");
+    }, 300);
+  } else {
+    navLinks.classList.add("active");
+  }
 
   menuButton.classList.toggle("fa-plus");
   menuButton.classList.toggle("fa-xmark");
